e33777c66c8c7de02a8d5eaea89c4d0b7101861f
ari123rm <arielmoraes05@gmail.com>
2024-11-19 17:48:52 -0300

    feat: add login path

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import loginProps from "./loginProps";
+import style from "./themes/loginstyle.module.scss";
+const Login:React.FC<loginProps> = ({tipo})=>{
+
+    return (
+        <>
+        <header>
+            
+        </header>
+        <main className={style.login}>  
+            
+        </main>
+        </>
+    )
+}
+
+export default Login;
