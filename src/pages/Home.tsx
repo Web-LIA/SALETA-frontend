ff43a5db5c87bf2c7f67a19cc8510cfe38f3695e
Andrededede <andrelucas1708@gmail.com>
2024-11-19 16:38:39 -0300

    feat: adicionar estrutura da home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 function Home(){
     return (
-        <>
-        
-        </>       
+        <div className='home'>
+            <img src="" alt="?" className='sobre'/>
+            <a href="" className='itemHome'>
+                <img src="" alt="ENCONTRAR" />
+                <p>Encontrar Item</p>
+            </a>
+            <a href="" className='itemHome'>
+                <img src="" alt="GUARDAR" />
+                <p>Guardar Item</p>
+            </a>
+        </div>     
     )
 }
 
