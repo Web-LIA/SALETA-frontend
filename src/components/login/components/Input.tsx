cd66937a851696bc9aa9f20338a8d9e0ac290d5c
Ariel Rocha <141646190+ari123rm@users.noreply.github.com>
2024-11-20 04:42:26 -0300

    Revert "Revert "Login/Visitante""

---
+++ src/components/login/components/Input.tsx
@@ -0,0 +1,56 @@
+import { useContext, useState } from "react";
+import { IoMdEye,IoIosEyeOff } from "react-icons/io";
+import style from "./input.module.scss";
+import  {ContextLogin} from "../Login";
+import { ContextVisitante } from "../Visitante";
+interface inputProps{
+    label:string;
+    type:'text'|"number"|"password";
+    name:string;
+    set:string;
+    url:"login"|"visitante";
+    required?:boolean;
+    title?:string;
+    placeholder?:string;
+    maxLength?:number;
+    value?:number|string;
+}
+const Input:React.FC<inputProps> = ({label,type,name,title,placeholder,set,required,url,maxLength,value}) =>{
+    const [revelar,setRevelar] = useState<boolean>(false);
+    let contexts = {
+        login: useContext(ContextLogin), 
+        visitante:useContext(ContextVisitante)
+    }
+    const context = contexts[url];
+
+
+   
+    
+
+    return (
+    <div className={style.input}>
+        <label htmlFor={name}>{label}</label>
+        {type == "password" ? (
+            <div className={style.eye} >
+                {revelar ?
+                
+                (
+                    <>
+                        <input type="text" title={title} placeholder={placeholder} name={name} id = {name} onChange={e=>{context[`set${set}`](e.target.value)}} required = {required}  maxLength = {maxLength} value={value}/>
+                        <button onClick={() => {setRevelar(false)}} title="eye open" type="button"><IoMdEye/></button>
+                    </>
+                ):(
+                    <>
+                        <input type="password" title={title} placeholder={placeholder} name={name} id = {name} onChange={e=>{context[`set${set}`](e.target.value)}} required = {required}  maxLength = {maxLength} value={value}/>
+                        <button onClick={() => {setRevelar(true)} }title="eye close" type="button"><IoIosEyeOff/></button>
+                    </>
+                )}
+                
+            </div>
+        ):(
+            <input type={type} title={title} placeholder={placeholder} name={name} id = {name} onChange={e=>{context[`set${set}`](e.target.value)}} required = {required}  maxLength = {maxLength} value={value}/>
+        )}
+    </div>
+    )
+}
+export default Input;
