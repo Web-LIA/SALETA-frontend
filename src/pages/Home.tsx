7b723596fcbae3cedd5a7c4e76fd88cd645b9af3
Andrededede <andrelucas1708@gmail.com>
2025-01-07 23:16:51 -0300

    fix: adicionar componente Header na pagina Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,20 +2,25 @@ import React from 'react'
 import imagemEncontrar from '../assets/home/EncontrarItem.png'
 import imagemGuardar from '../assets/home/GuardarItem.png'
 import themes from '../themes/home.module.scss'
+import Header from '../components/Header'
 
 function Home(){
     return (
-        <div className={themes.home}>
-            <img src="" alt="?" className={themes.sobre}/>
-            <a href="/itens" className={themes.encontrar}>
-                <img src={imagemEncontrar} alt="ENCONTRAR" />
-                <p>Encontrar Item</p>
-            </a>
-            <a href="/login"  className={themes.guardar}>
-                <img src={imagemGuardar} alt="GUARDAR" />
-                <p>Guardar Item</p>
-            </a>
-        </div>     
+        <>
+            <Header titulo='S.A.L.E.T.A.'/>
+            <div className={themes.home}>
+                <img src="" alt="?" className={themes.sobre}/>
+                <a href="/itens" className={themes.encontrar}>
+                    <img src={imagemEncontrar} alt="ENCONTRAR" />
+                    <p>Encontrar Item</p>
+                </a>
+                <a href="/login"  className={themes.guardar}>
+                    <img src={imagemGuardar} alt="GUARDAR" />
+                    <p>Guardar Item</p>
+                </a>
+            </div>     
+        </>    
+        
     )
 }
 
