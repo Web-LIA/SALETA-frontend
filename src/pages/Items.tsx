a4ee2227e0b47cc95af811dfec856ee9c0a88bce
Andrededede <andrelucas1708@gmail.com>
2025-02-21 18:15:56 -0300

    feat: Items responsivo mobile e pc

--- src/pages/Items.tsx
+++ src/pages/Items.tsx
@@ -40,7 +40,8 @@ function Items() {
             <main className={themes.itemMain}>
                 {itemListFiltered.map(item => (
                     <div className={themes.itemCard}>
-                        <img src={item.photo} alt={item.title} />
+                        <div className={themes.itemInfo}>
+                            <img src={item.photo} alt={item.title} />
                             <div className={themes.itemContent}>
                                 <h3>{item.title}</h3>
                                 <p className={themes.itemDesc}>{item.description}</p>
@@ -50,7 +51,8 @@ function Items() {
                                 </div>
                                 <p className={themes.itemDate}>{format(item.date, 'dd/MM/yyyy HH:mm')}</p>
                             </div>
-                            <button onClick={start_session}>Recuperar Item</button>
+                        </div>
+                        <button onClick={start_session} className={themes.itemButton}>Recuperar Item</button>
                     </div>
                 ))}
             </main>
