78aeade2b00f1f5d38de95d2e1f32cd9bb0b7a7a
ari123rm <141646190+ari123rm@users.noreply.github.com>
2025-07-08 03:19:09 -0300

    feat: novo backend adicionado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 //export const API_URL = 'http://localhost:3333';
- export const API_URL = 'https://saleta-backend.vercel.app';
+//export const API_URL = 'https://saleta-backend.vercel.app';
+export const API_URL = 'https://saleta-backend.lostbytes.me';
 //export const API_URL = 'http://192.168.3.74:3333'
 
 const api = axios.create({
